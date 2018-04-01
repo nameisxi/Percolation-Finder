a019a4a8435b06e5f5314433a6cb5e3aa2c0b70e
nameisxi <tuomas.j.j.peltonen@gmail.com>
2018-04-01 16:11:45 +0300

    Updated drawing

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import pygame
 import random
 
+from pygame.locals import *
 from quickunionfind import QuickUnionFind
 from percolation import Percolation
 
@@ -14,14 +15,14 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 
 # Site dimensions
-WIDTH = 15
-HEIGHT = 15
+WIDTH = 6
+HEIGHT = 6
 MARGIN = 1
 
 # Grid initialization
 grid = []
-rows = 50
-columns = 50
+rows = 80
+columns = 80
 for row in range(rows):
     grid.append([])
     for column in range(columns):
@@ -30,13 +31,24 @@ random_rows = random.sample(range(0, rows), rows)
 random_columns = random.sample(range(0, columns), columns)
 
 # Pygame initialization
-WINDOW_SIZE = [(columns * WIDTH + (MARGIN * columns) + MARGIN), (rows * HEIGHT + (MARGIN * rows) + MARGIN)]
+WINDOW_SIZE = [(columns * WIDTH + (MARGIN * columns) + MARGIN), (rows * HEIGHT + (MARGIN * rows) + MARGIN) + 20]
 screen = pygame.display.set_mode(WINDOW_SIZE)
-pygame.display.set_caption("Percolation Threshold Checker")
+pygame.display.set_caption("Percolation Finder")
 clock = pygame.time.Clock()
 pygame.init()
 
 percolation = Percolation(rows) 
+
+# Does it percolate label initialization
+does_it_percolate_text = pygame.font.SysFont("monospace", 12)
+does_it_percolate_label = does_it_percolate_text.render("Percolates: False", 1, (255,255,255))
+screen.blit(does_it_percolate_label, (50, 565))
+
+# Number of open sites label initialization
+number_of_open_sites_text = pygame.font.SysFont("monospace", 12)
+number_of_open_sites_label = number_of_open_sites_text.render("Number of open sites: 0", 1, (255,255,255))
+screen.blit(number_of_open_sites_label, (200, 565))
+
 done = False
 
 while not done:
@@ -57,20 +69,23 @@ while not done:
     for row in range(rows):
         for column in range(columns):
             color = BLACK
-            if grid[row][column] == 1:
-                if row > 0 and column > 0 and percolation.is_full(row, column):
+            if grid[row][column] == 1:  
+                if percolation.is_full(row, column):
                     percolation.open_site(row, column)    
                     color = BLUE 
-                elif row > 0 and column > 0:
-                    percolation.open_site(row, column)     
-                    color = WHITE         
-                else:       
-                    color = WHITE   
-
-                #if percolation.percolates:
-                #    color = GREEN         
+                else:
+                   percolation.open_site(row, column)     
+                   color = WHITE           
+       
             pygame.draw.rect(screen, color, [(MARGIN + WIDTH) * column + MARGIN, (MARGIN + HEIGHT) * row + MARGIN, WIDTH, HEIGHT])
+    
+    if percolation.percolates:
+        does_it_percolate_label = does_it_percolate_text.render("Percolates: True", 1, (255,255,255))
+        screen.blit(does_it_percolate_label, (50, 565))
 
+    number_of_open_sites_label = number_of_open_sites_text.render("Number of open sites: " + str(percolation.get_number_of_open_site()), 1, (255,255,255))
+    screen.blit(number_of_open_sites_label, (200, 565))    
+    
     # FPS
     clock.tick(60)
     pygame.display.flip()
--- percolation.py
+++ percolation.py
@@ -17,53 +17,55 @@ class Percolation(object):
 
 
     def is_open(self, i, j):
-        if i < 1 or i > self.width or j < 1 or j > self.width:
-            raise IndexError
+        if i < 1 or j < 1:
+            site_to_open = i * self.width + j
+            return self.sites_states[site_to_open]
+
         site_to_open = (i - 1) * self.width + j
         return self.sites_states[site_to_open]
 
 
     def open_site(self, i, j):
-        if i <= 0 or j <= 0 or i > self.width or j > self.width:
-            raise IndexError
-
         if self.is_open(i, j):
             return     
 
         site_to_open = (i - 1) * self.width + j
+
+        if i < 1 or j < 1:
+            site_to_open = i * self.width + j
+        else:
+            if j != 1 and self.is_open(i, j - 1):
+                self.union_find.union(site_to_open - 1, site_to_open)
+                self.union_find_adjustor.union(site_to_open - 1, site_to_open)
+        
+            if j != self.width and self.is_open(i, j + 1):
+                self.union_find.union(site_to_open + 1, site_to_open)
+                self.union_find_adjustor.union(site_to_open + 1, site_to_open)
+        
+            if i != 1 and self.is_open(i - 1, j):
+                self.union_find.union(site_to_open - self.width, site_to_open)
+                self.union_find_adjustor.union(site_to_open - self.width, site_to_open)
+        
+            if i != self.width and self.is_open(i + 1, j):
+                self.union_find.union(site_to_open + self.width, site_to_open)
+                self.union_find_adjustor.union(site_to_open + self.width, site_to_open)
+        
+            if site_to_open <= self.width:
+                self.union_find.union(0, site_to_open)
+                self.union_find_adjustor.union(0, site_to_open)
+        
+            if site_to_open > self.number_of_sites - self.width:
+                self.union_find.union(self.number_of_sites + 1, site_to_open)
+
         self.sites_states[site_to_open] = 1
         self.number_of_open_sites = self.number_of_open_sites + 1
 
-        if j != 1 and self.is_open(i, j - 1):
-            self.union_find.union(site_to_open - 1, site_to_open)
-            self.union_find_adjustor.union(site_to_open - 1, site_to_open)
-        
-        if j != self.width and self.is_open(i, j + 1):
-            self.union_find.union(site_to_open + 1, site_to_open)
-            self.union_find_adjustor.union(site_to_open + 1, site_to_open)
-        
-        if i != 1 and self.is_open(i - 1, j):
-            self.union_find.union(site_to_open - self.width, site_to_open)
-            self.union_find_adjustor.union(site_to_open - self.width, site_to_open)
-        
-        if i != self.width and self.is_open(i + 1, j):
-            self.union_find.union(site_to_open + self.width, site_to_open)
-            self.union_find_adjustor.union(site_to_open + self.width, site_to_open)
-        
-        if site_to_open <= self.width:
-            self.union_find.union(0, site_to_open)
-            self.union_find_adjustor.union(0, site_to_open)
-        
-        if site_to_open > self.number_of_sites - self.width:
-            self.union_find.union(self.number_of_sites + 1, site_to_open)
-        
-
 
     def is_full(self, i, j):
-        if i < 1 or i > self.width or j < 1 or j > self.width:
-            raise IndexError
-
         site_to_open = (i - 1) * self.width + j
+        if i < 1 or j < 1:
+            site_to_open = i * self.width + j
+
         return self.union_find.connected(0, site_to_open) and self.union_find_adjustor.connected(0, site_to_open)
 
 
